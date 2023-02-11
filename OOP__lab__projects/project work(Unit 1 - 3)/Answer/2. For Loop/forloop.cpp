c7c7610b4af7943fe78e98873b944b7b01baed11
AnukulPoudel <anukulpoudel999@gmail.com>
2023-02-11 19:54:26 +0545

    2. for loop

---
+++ OOP__lab__projects/project work(Unit 1 - 3)/Answer/2. For Loop/forloop.cpp
@@ -0,0 +1,14 @@
+//program that uses a for loop to output the numbers from 1 to 10.
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int i;
+    for(i=1;i<11;i++){
+        cout<<i<<endl;  //endl breaks line and flushes unlike '\n'
+    }
+    return 0;
+}
